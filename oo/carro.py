4091fbf1cccc047bd68f114a236e70986bfad01f
Alan <almorato@gmail.com>
2021-12-30 14:50:43 -0300

    Criação da classe Direcao, seus atributos e métodos.

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,37 @@
+NORTE = 'Norte'
+SUL = 'Norte'
+LESTE = 'Norte'
+OESTE = 'Norte'
+
+
+class Direcao:
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
+
 class Motor:
     def __init__(self):
         self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)  # não permite velocidades negativas
+
+
+
